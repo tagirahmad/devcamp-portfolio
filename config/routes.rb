6ebf51218cd8a7a8b3d9f14809c6bc83549537cd
tagirahmad <tagirbpl@gmail.com>
2021-01-17 09:49:56 +0300

    Integrated skills via model generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages#home'
-  get 'pages#about'
-  get 'pages#contact'
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
   resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
