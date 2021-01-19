430d3db9db6cbff1725a91469e9d184e7222e836
tagirahmad <tagirbpl@gmail.com>
2021-01-19 19:18:40 +0300

    Integrated friendly routes for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, expect: [:show]
+
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  
+
   resources :blogs
   root to: 'pages#home'
 end
