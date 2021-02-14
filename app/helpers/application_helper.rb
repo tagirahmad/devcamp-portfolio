c41c12544ba76757a249881c98ddcadff8d2cff1
tagirahmad <tagirbpl@gmail.com>
2021-02-14 18:50:41 +0300

    Fixed bug with login helper method by setting defaults

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def login_helper(cls: 'nav-link')
+  def login_helper(cls: '')
     if current_user.is_a? GuestUser
       (link_to 'Register', new_user_registration_path, class: cls) +
           (link_to 'Login', new_user_session_path, class: cls)
