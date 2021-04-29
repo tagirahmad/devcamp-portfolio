cf5f2703e8988f236e8eff4a584173117e97ee14
tagirahmad <tagirbpl@gmail.com>
2021-04-29 09:26:35 +0300

    Add jquery-ui; sortable html5 & css calls

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -14,6 +14,7 @@
 //= require jquery3
 //= require jquery_ujs
 //= require popper
+//= require jquery-ui
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
+require("packs/html.sortable");
 
 Rails.start()
 Turbolinks.start()
